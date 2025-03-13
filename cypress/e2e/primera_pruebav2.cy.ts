a50bed29efed33b9d503f3c6def1f757c3fe672e
HazelLoya <hloya@arkusnexus.com>
2025-03-13 15:32:55 -0700

    Se agregaron identificadores

--- cypress/e2e/primera_pruebav2.cy.ts
+++ cypress/e2e/primera_pruebav2.cy.ts
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get('h1').should('contain','Lista de Actividades')
+    cy.get('h1',{timeout:10_000}).should('contain','Lista de Actividades');
+    cy.contains('Agregar').click();
   })
 })
